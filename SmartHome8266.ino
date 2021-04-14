4468239bef456f0d48ce2ad984d446fb09b90578
danishjoseph <56467833+danishjoseph@users.noreply.github.com>
2021-04-14 22:23:13 +0530

    Update SmartHome8266.ino

--- SmartHome8266.ino
+++ SmartHome8266.ino
@@ -7,12 +7,14 @@
 
 #define DEVICELENGTH 4 // maximum 4 devices can be added in ESP8266
 
-const char* ssid = "SSID";                                        // wifi name
-const char* password = "PASSWORD";                                // wifi password
-const char* mqtt_server = "MQTT_SERVER";                          // mqtt server name
-String device[DEVICELENGTH] = {"esp01","esp02","esp03","esp04"};  // change the device name to your liking but device id must be unique !!
-int relays[DEVICELENGTH] = { 10,13,12,14 };                       // GPIO pins to be connected in the relay
-int switches[DEVICELENGTH] = { 9,4,5,3 };                         // Switch inputs
+const char* ssid = "wifi";
+const char* password = "password";
+const char* mqtt_server = "0.0.0.0";  // mosquitto broker IP
+const char* roomName = "bedroom";   // esp name
+int mqttPort = 1883; // mqtt port default is 1883
+String device[DEVICELENGTH] = {"b11","b12","b13","b14"}; // change the device name to your liking but device id must be unique !!
+int relays[DEVICELENGTH] = { 10,13,12,14 };                        // GPIO pins to be connected in the relay
+int switches[DEVICELENGTH] =  {4,1,3,5};                           // GPIO pins to be connected in the switch
 int tempState[DEVICELENGTH];
 int previousState[DEVICELENGTH];
 int i;
@@ -22,28 +24,32 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
-  Serial.begin(115200);
+  // Serial.begin(115200);
   EEPROM.begin(4);
-  Serial.println("SETUP STARTS");
+  //GPIO 1 (TX) swap the pin to a GPIO.
+pinMode(1, FUNCTION_3);
+//GPIO 3 (RX) swap the pin to a GPIO.
+pinMode(3, FUNCTION_3);
+  // Serial.println("SETUP STARTS");
   for (i = 0; i < DEVICELENGTH; i++) {
     pinMode(relays[i], OUTPUT);          // Declaring relay pins as output
     pinMode(switches[i], INPUT_PULLUP);  //Declaring switch pins as input (INPUT_PULLUP: For eliminating AC interference)
     digitalWrite(relays[i], 1);          //Set relay OFF initially (0=ON, 1=OFF)
     tempState[i] = EEPROM.read(i);
-    Serial.println("SETUP STARTS:" + tempState[i] );
+    // Serial.println("SETUP STARTS:" + tempState[i] );
     wifiLogic(tempState[i],relays[i],i);
     }
 
-  Serial.println("Booting");
-  client.setServer(mqtt_server, 1883);
+  // Serial.println("Booting");
+  client.setServer(mqtt_server, mqttPort);
   client.setCallback(callback);
   lastReconnectAttempt = 0;             //mqtt code
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+    // Serial.println("Connection Failed! Rebooting...");
+    for(i = 0; i < DEVICELENGTH; i++)
+       tempState[i] = switchStates(switches[i],relays[i],i,tempState[i]);
   }
 
   ArduinoOTA.onStart([]() {
@@ -54,32 +60,33 @@ void setup() {
       type = "filesystem";
     }
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
+    // Serial.println("Start updating " + type);
   });
   ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
+    // Serial.println("\nEnd");
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    // Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
+    // Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
+      // Serial.println("Auth Failed");
     } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
+      // Serial.println("Begin Failed");
     } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
+      // Serial.println("Connect Failed");
     } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
+      // Serial.println("Receive Failed");
     } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
+      // Serial.println("End Failed");
     }
   });
+  ArduinoOTA.setHostname(roomName);
   ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  // Serial.println("Ready");
+  // Serial.print("IP address: ");
+  // Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -88,7 +95,7 @@ void loop() {
     for(i = 0; i < DEVICELENGTH; i++)
        tempState[i] = switchStates(switches[i],relays[i],i,tempState[i]);
     long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
+    if (now - lastReconnectAttempt > 60000) {
       lastReconnectAttempt = now;
       // Attempt to reconnect
       if (reconnect()) {
@@ -122,11 +129,11 @@ void subscription(String deviceId,int relay)
 }
 
 boolean reconnect() {
-  String clientId = "ESP8266Client-";
+  String clientId = roomName;
   clientId += String(random(0xffff), HEX);
   if (client.connect(clientId.c_str())) {
     // Once connected, publish an announcement...
-    Serial.println("MQTT connected");
+    // Serial.println("MQTT connected");
     for(i = 0; i < DEVICELENGTH; i++)
        subscription(device[i],relays[i]);
 
@@ -140,10 +147,10 @@ boolean reconnect() {
 void callback(char* topic, byte* payload, unsigned int length) {
   // handle message arrived
   char message = payload[0];                        //convert byte to char
-  Serial.print("Topic from MQTT broker is : ");
-  Serial.print(topic);
-  Serial.print(" ");
-  Serial.println(message);
+  // Serial.print("Topic from MQTT broker is : ");
+  // Serial.print(topic);
+  // Serial.print(" ");
+  // Serial.println(message);
   for(i = 0; i < DEVICELENGTH; i++) {
    //if(strstr(topic,(char*) device[i].c_str()) != NULL)
      tempState[i] = compareTopic(topic,message,device[i],relays[i],i);
@@ -170,9 +177,9 @@ int compareTopic(String topic,char message, String deviceId,int relay,int addr)
 //To operate relay using WiFi
 void wifiLogic(bool wifiState,int relay,int addr)
 {
-  Serial.println("inside wifiState func");
-  Serial.println(wifiState);
-  Serial.println(digitalRead(relay));
+  // Serial.println("inside wifiState func");
+  // Serial.println(wifiState);
+  // Serial.println(digitalRead(relay));
   if(wifiState == true && digitalRead(relay))
     {
       digitalWrite(relay, 0);  //Relay on
@@ -189,26 +196,27 @@ void wifiLogic(bool wifiState,int relay,int addr)
 
 //For publishing the current state of relay through MQTT 
 void currentState(String deviceId,int relay) 
-{ 
+{     
       String publishtTopic = "device/" + deviceId + "/state";
       String currentState = digitalRead(relay) == 0 ? "1" : "0";
       client.publish((char*) publishtTopic.c_str(), (char*) currentState.c_str());
-      Serial.println("currentState published, deviceId:" + deviceId);
+      // Serial.println("currentState published, deviceId:" + deviceId);
 }
 
 //For publishing the state of relay through MQTT 
 int publishRelayChange(String deviceId,int relay,int previousState,int i)
 {
+  delay(30);
   int relayState = digitalRead(relay); 
   if(relayState != previousState)
   {
     String currentState = digitalRead(relay) == 0 ? "1" : "0";
     String topic = "device/" + deviceId + "/state";
     client.publish((char*) topic.c_str(), (char*) currentState.c_str());
-    Serial.println(currentState + " published, deviceId: " + deviceId + "relay: " + relay);
+    // Serial.println(currentState + " published, deviceId: " + deviceId + "relay: " + relay);
     digitalRead(relay) == 0 ? EEPROM.write(i, HIGH) : EEPROM.write(i, LOW);
     if (EEPROM.commit())
-      Serial.println("EEPROM successfully committed");
+      // Serial.println("EEPROM successfully committed");
     previousState = relayState;
   }  
   return previousState;
@@ -219,6 +227,7 @@ int switchStates(int button,int relay,int addr,int tempState)
 {
    if(digitalRead(button) == LOW)
     {
+      
       if(tempState == HIGH)
       { 
        digitalWrite(relay, 0);           //Relay on
@@ -227,11 +236,12 @@ int switchStates(int button,int relay,int addr,int tempState)
     }
     if(digitalRead(button) == HIGH )
     {
+       
       if(tempState == LOW)
       {
         digitalWrite(relay, 1);         //Relay off
       }
         return HIGH;
     }
-  delay(100);
+  //delay(100);
 }
